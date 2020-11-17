e14b0baadcb62dd8a8b21384ada4e899a4f6c149
Hans Mose Jensen <hans.jensen@ices.dk>
2020-11-17 15:17:20 +0100

    adding 2021 assessment and .gitattributes (lfs). RData object tracked with lfs

---
+++ 2021/assessment_platform.R
@@ -0,0 +1,20 @@
+require(lattice)
+require(grid)
+require(rmarkdown)
+require(dplyr)
+require(data.table)
+
+series <- commandArgs(trailingOnly = TRUE)[1]
+compartment <- commandArgs(trailingOnly = TRUE)[2]
+
+load("assessment_platform.RData")
+
+render(
+  output_file = paste(gsub(" ", "_", series), "__", gsub(" ", "_", compartment), "__2021.html", sep = ""),
+  output_dir = "html",
+  "assessment platform.Rmd",
+  params = list(
+    compartment = compartment,
+    series = series
+  )
+)
