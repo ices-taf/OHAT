3a9a216583ed038b454935928fee99a7dafb59fb
Hans Mose Jensen <hans.jensen@ices.dk>
2022-10-04 12:13:08 +0200

    adding some test series etc

--- 2022/assessment_platform.R
+++ 2022/assessment_platform.R
@@ -22,20 +22,28 @@ render(
 
 #Use this for executing
 if (FALSE) {
+  #    series = "Norway_97A2 Bod? harbour AS Mytilus edulis SB Not_applicable",
+  #    series = "Sweden_V?der?arna CD Clupea harengus LI Not_applicable",
+  #   series = "Norway_57A Krossanes CD Mytilus edulis SB",
+  #   series = "Norway_91A2 ?rland ytre Trondheimsfjord CD Mytilus edulis SB Not_applicable"
+  #   Sediment
+  #   series = "Germany_STG16 CD"
+  #   Water
+  #   series = "The Netherlands_DOOVBWT CD"
+  #   series = "Ireland_Carlingford Lough Inner Stn 2 CD"
   
   params <- list(
-    series = "Norway_97A2 Bodø harbour AS Mytilus edulis SB Not_applicable",
-#    series = "Norway_57A Krossanes CD Mytilus edulis SB",
-    compartment = "biota"
+
+   series = "Germany_STG16 CD",
+    compartment = "sediment"
   )
   
-  rm(params)
+#  rm(params)
   render(
     "assessment_platform.Rmd",
-    params = list(
+      params = list(
       compartment = "biota",
-      series = "Norway_97A2 Bodø harbour AS Mytilus edulis SB Not_applicable"
-#      series = "Norway_57A Krossanes CD Mytilus edulis SB"
+      series = "Sweden_VÃ¤derÃ¶arna CD Clupea harengus LI Not_applicable"
     )
   )
 }
