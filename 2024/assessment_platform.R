b806d5d8e50caf5a7bb31a8f6252eb420f466316
Hans Mose Jensen <hans.jensen@ices.dk>
2023-11-15 15:13:07 +0100

    1st draft 2024 assessmnet from Rob (20231114)

---
+++ 2024/assessment_platform.R
@@ -0,0 +1,49 @@
+require(lattice)
+require(grid)
+require(rmarkdown)
+require(dplyr)
+require(data.table)
+
+series <- commandArgs(trailingOnly = TRUE)[1]
+compartment <- commandArgs(trailingOnly = TRUE)[2]
+
+load("assessment_platform.RData")
+
+render(
+  output_file = paste(gsub(" ", "_", series), "__", gsub(" ", "_", compartment), "__2024.html", sep = ""),
+  output_dir = "html",
+  "assessment_platform.Rmd",
+  params = list(
+    compartment = compartment,
+    series = series
+  )
+)
+
+
+#Use this for executing
+if (FALSE) {
+  #    series = "Norway_97A2 Bod? harbour AS Mytilus edulis SB Not_applicable",
+  #    series = "Sweden_V?der?arna CD Clupea harengus LI Not_applicable",
+  #   series = "Norway_57A Krossanes CD Mytilus edulis SB",
+  #   series = "Norway_91A2 ?rland ytre Trondheimsfjord CD Mytilus edulis SB Not_applicable"
+  #   Sediment
+  #   series = "Germany_STG16 CD"
+  #   Water
+  #   series = "The Netherlands_DOOVBWT CD"
+  #   series = "Ireland_Carlingford Lough Inner Stn 2 CD"
+  
+  # params <- list(
+
+  # series = "Norway_10A2 Skallneset AG Mytilus edulis SB Not_applicable",
+  #  compartment = "biota"
+  #)
+  
+#  rm(params)
+  render(
+    "assessment_platform.Rmd",
+      params = list(
+      compartment = "biota",
+      series = "2958 CD Gadus morhua LI"
+    )
+  )
+}
