cc3d0d94d170611f708789cd64e7c3a83b1a997b
colin millar <colinpmillar@gmail.com>
2026-03-06 16:17:33 +0100

    add 2020

---
+++ 2020/assessment_platform.R
@@ -0,0 +1,39 @@
+require(lattice)
+require(grid)
+require(rmarkdown)
+require(dplyr)
+require(data.table)
+
+series <- commandArgs(trailingOnly = TRUE)[1]
+compartment <- commandArgs(trailingOnly = TRUE)[2]
+
+load("assessment_platform.RData")
+
+render(
+  output_file = paste(gsub(" ", "_", series), "__", gsub(" ", "_", compartment), "__2020.html", sep = ""),
+  output_dir = "html",
+  "assessment_platform.Rmd",
+  params = list(
+    compartment = compartment,
+    series = series
+  )
+)
+
+
+#Use this for executing
+if (FALSE) {
+  
+  params <- list(
+    series = "Norway_57A Krossanes CD Mytilus edulis SB",
+    compartment = "biota"
+  )
+  
+  rm(params)
+  render(
+    "assessment_platform.Rmd",
+    params = list(
+      compartment = "biota",
+      series = "Norway_57A Krossanes CD Mytilus edulis SB"
+    )
+  )
+}
